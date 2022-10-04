bbc31171504fded2b77ce251d04a8bd4de59b4ab
MaksMalik <maksymilian.malik@gmail.com>
2022-10-04 16:13:57 +0200

    Create app, add components, add reset

---
+++ future-one/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './Components/Header/Header';
+import StartPage from './Components/StartPage/StartPage';
+import DropDown from './Components/DropDown/DropDown';
+import Quote from './Components/Quote/Quote';
+import MoreInfo from './Components/MoreInfo/MoreInfo';
+import Newsletter from './Components/Newsletter/Newsletter';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <StartPage />
+      <DropDown />
+      <Quote/>
+      <MoreInfo />
+      <Newsletter/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
