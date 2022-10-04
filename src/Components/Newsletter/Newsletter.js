c47910d7688dfae2526b75df4e3bc972762b6818
MaksMalik <maksymilian.malik@gmail.com>
2022-10-05 01:01:20 +0200

    add more breakpointes, add component more info

--- src/Components/Newsletter/Newsletter.js
+++ src/Components/Newsletter/Newsletter.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Newsletter() {
   return (
-    <div>Newsletter</div>
+    <>
+      <div className='Newsletter'>
+        <div className='Newsletter__title'>Subscribe to our newsletter</div>
+
+      </div>
+    
+    </>
   )
 }
