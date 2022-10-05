4859c2154237ecec12a209fbbc795e51b0c66990
MaksMalik <maksymilian.malik@gmail.com>
2022-10-05 14:22:47 +0200

    Add working button to dropdown

--- src/Components/DropDown/DropDown.js
+++ src/Components/DropDown/DropDown.js
@@ -9,39 +9,32 @@ export default function DropDown() {
         <div className='DropDown__tile' id='discover'>
           Lacinia quis vel eros tempor orci.
         </div>
-
         <div className='DropDown__drop'>
-          <div className='DropDown__drop__element'>
-            <div className='DropDown__drop__element__left'>
-              <div className='DropDown__drop__element__left__number'>01.</div>
-              <div className='DropDown__drop__element__left__title'>Arcu non odio</div>
+          <div className='DropDown__drop__element__left__number'>01.</div>
+          <div className="DropDown__drop__element">
+            <details close>
+              <summary>Arcu non odio</summary>
               <div className='DropDown__drop__element__left__subtitle'>Sapien nec sagittis aliquam malesuada bibendum. Tempor nec feugiat nisl pretium.</div>
-            </div>
-            <div className='DropDown__drop__element__right__minus'></div>
+            </details>
           </div>
 
-          <div className='DropDown__drop__element'>
-            <div className='DropDown__drop__element__left'>
-              <div className='DropDown__drop__element__left__number'>02.</div>
-              <div className='DropDown__drop__element__left__title'>Eget gravida cum sociis</div>
-              <div className='DropDown__drop__element__left__subtitle'>Curabitur in ligula dictum, hendrerit est eget, pretium lorem.</div>
-            </div>
-            <div className='DropDown__drop__element__right__minus'></div>
+          <div className='DropDown__drop__element__left__number'>02.</div>
+          <div className="DropDown__drop__element">
+            <details close>
+              <summary>Eget gravida cum</summary>
+              <div className='DropDown__drop__element__left__subtitle'>Curabitur in ligula dictum, hendrerit est eget, pretium lorem.
+</div>
+            </details>
           </div>
 
-          <div className='DropDown__drop__element'>
-            <div className='DropDown__drop__element__left'>
-              <div className='DropDown__drop__element__left__number'>03.</div>
-              <div className='DropDown__drop__element__left__title'>Massa massa ultricies </div>
-              <div className='DropDown__drop__element__left__subtitle'>Donec aliquam, velit venenatis eleifend tincidunt.</div>
-            </div>
-            <div className='DropDown__drop__element__right__minus'></div>
+          <div className='DropDown__drop__element__left__number'>03.</div>
+          <div className="DropDown__drop__element">
+            <details close>
+              <summary>Massa massa</summary>
+              <div className='DropDown__drop__element__left__subtitle'>Donec aliquam, velit venenatis eleifend tincidunt.
+</div>
+            </details>
           </div>
-
-
-
-
-
         </div>
       </div>
     </>
